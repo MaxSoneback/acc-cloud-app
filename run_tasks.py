fe61b62de3fe8175c87ae0c8e33615d2536f315d
Max Sonebäck <maxsoneback@hotmail.com>
2020-10-07 12:36:54 +0000

    progress

--- run_tasks.py
+++ run_tasks.py
@@ -1,15 +1,8 @@
 import glob
 from celery import group
 import tasks
-def read_file(file):
-    for line in file:
-        # split all tweets to single tweet
-        yield line
 
 filepath = './data/*'
 files = glob.glob(filepath)
-
-for file in files[0:1]:
-    data = read_file(open(file, "r"))
-    tweets = group(tasks.get_valid_json.s(line) for line in data)()
-    tweets.get()
+data = tasks.extract_tweets.delay(files[0])
+data.get()
--- tasks.py
+++ tasks.py
@@ -1,20 +1,33 @@
 from collections import Counter
 from cel import app
+from celery import group
 import json
 from json import JSONDecodeError
 
 @app.task
-def get_valid_json(line):
+def extract_tweets(filepath):
+    f = open(filepath, "r")
+    res = []
+    for line in f:
+        tweet = (validate_line.s(line) | remove_retweet.s())()
+        res.append(tweet)
+    return group(res)
+
+@app.task(bind=True)
+def validate_line(self, line):
     try:
         valid_json = json.loads(line)
         return valid_json
     except JSONDecodeError:
-        pass
+        self.request.callbacks = None
 
-@app.task
-def get_tweets(doc):
-    for line in doc:
-        yield line
+@app.task(bind=True)
+def remove_retweet(self, tweet):
+    try:
+        tweet["retweeted_status"]
+        self.request.callbacks = None
+    except KeyError:
+        return tweet
 
 @app.task
 def map(tweet):

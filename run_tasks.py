36d19cd3660d646ace4b5ad85a1594453eec61ef
max sonebäck <maxsoneabck@hotmail.com>
2020-10-08 15:38:54 +0000

    s

--- run_tasks.py
+++ run_tasks.py
@@ -2,27 +2,26 @@ import glob
 from celery import group, chain, chord
 import tasks
 
-def extract_and_map_tweets(filepath):
-    f = open(filepath, "r")
+def extract_and_map_tweets(filepaths, num_of_files):
+    #save all chain signatures
+    chains=[]
+    for filepath in filepaths[0:num_of_files]:
+        f = open(filepath, "r")
 
-    #Save list of all chains for a file
-    chains = []
-    for line in f:
-        #Validate that the line is a json-object, only save non-retweets and map all pronouns
-        tweet = chain(tasks.validate_line.s(line) | tasks.remove_retweet.s() | tasks.map.s() )
-        chains.append(tweet)
-    print('returning list of chains')
-    return chains
+        for line in f:
+            #Validate that the line is a json-object, only save non-retweets and map all pronouns
+            tweet = chain(tasks.validate_line.s(line) | tasks.remove_retweet.s() | tasks.map.s() )
+            chains.append(tweet)
+
+    return group(*chains)
 
 #Create list of all filepaths
 filepath = './data/*'
 files = glob.glob(filepath)
-print('extracting and mapping tweets')
 #Extract tweets from file and map them
-header = extract_and_map_tweets(files[0])
+header = extract_and_map_tweets(files, 5)
 print('header created')
 #Reduce the mappings
 callback = tasks.reduce.s()
-results = chord(header)(callback)
-print('end')
+results = (header | callback)()
 print(results.get())
--- tasks.py
+++ tasks.py
@@ -22,20 +22,22 @@ def validate_line(self, line):
         return valid_json
     except JSONDecodeError:
         # Only keep valid tweets, i.e. not new-lines or broken json objects
-        self.request.chain = None
+        #self.request.chain = self.requet.callbacks = None
+        return json.loads('{"text" : ""}')
 
 @app.task(bind=True, ignore_results=True)
 def remove_retweet(self, tweet):
     try:
         #If key 'retweeted_status' exists, discard the tweet
         tweet["retweeted_status"]
-        self.request.chain = None
+        return json.loads('{"text": ""}')
+        #self.request.chain = self.request.callbacks = None
     except KeyError:
         return tweet
 
-@app.task(bind=True)
-def map(self, tweet):
-    c = Counter()
+@app.task()
+def map(tweet):
+    c = {}
     matches = re.findall(rf"{pattern}", tweet["text"])
     if matches:
         #To count the total number of unique tweets, map the value 1 to the key "tweet" for each tweet
@@ -44,13 +46,11 @@ def map(self, tweet):
             if match.lower() not in c:
                 c[match.lower()] = 0
             c[match.lower()] += 1
-    else:
-        self.request.chain = None
     return c
 
 @app.task
 def reduce(counters):
-    res = counters[0]
-    for c in counters[1:]:
-        res += c
-    return res
+    res = Counter() 
+    for counter in counters:  
+            res.update(counter)
+    return json.dumps(res, indent=4)

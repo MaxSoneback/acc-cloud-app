6e4ccb12cf8fd1ad62f34f2df4cee94a3001184e
max sonebäck <maxsoneabck@hotmail.com>
2020-10-08 13:25:17 +0000

    c

--- cel.py
+++ cel.py
@@ -1,26 +1,6 @@
-import celery
-from celery import Celery, group, states
-from celery.backends.redis import RedisBackend
+from celery import Celery
 
-def patch_celery():
-    """Patch redis backend."""
-    def _unpack_chord_result(
-            self, tup, decode,
-            EXCEPTION_STATES=states.EXCEPTION_STATES,
-            PROPAGATE_STATES=states.PROPAGATE_STATES,
-                                                ):  
-        _, tid, state, retval = decode(tup)
-        
-        if state in EXCEPTION_STATES:
-            retval = self.exception_to_python(retval)
-            if state in PROPAGATE_STATES:
-                # retval is an Exception 
-                return '{}: {}'.format(retval.__class__.__name__, str(retval))
-            return retval
-        celery.backends.redis.RedisBackend._unpack_chord_result = _unpack_chord_result
-        return celery
-
-app = patch_celery().Celery('proj',
+app = Celery('proj',
         broker='amqp://',
         backend='redis://localhost:6379/0',
         include=['tasks'])
